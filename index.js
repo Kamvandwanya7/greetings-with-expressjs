0ad8a4ff4696eab8bd7e19cef1bd9d3ea6f4f0a8
Kamva <kayndwanya24@gmail.com>
2022-08-16 16:51:51 +0200

    third commit

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // should we use a SSL connection
-let useSSL = false;
-let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
-    useSSL = true;
-}
+// let useSSL = false;
+// let local = process.env.LOCAL || false;
+// if (process.env.DATABASE_URL && !local){
+//     useSSL = true;
+// }
 // which db connection to use
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/my_greetings';
 
+if (process.env.NODE_ENV == 'production') {
+	ssl = {
+		rejectUnauthorized : false
+	}
+}
 const pool = new Pool({
-    connectionString
-    // ssl : useSSL
+    connectionString,
+    ssl : useSSL
   });
 
 const app = express();
